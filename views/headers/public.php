f3d33a9032ae74dfc7512edb8e939854aa279fc5
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-07-03 10:42:10 +0530

    Change error page css and url for public header.

--- views/headers/public.php
+++ views/headers/public.php
@@ -4,6 +4,6 @@ use yii\helpers\Html;
 ?>
 <header class="header header-public">
     <div class="header-content clearfix">
-        <?=Html::a( "<img class='logo' src='" . Yii::getAlias( '@images' ) . "/logo.png'>", [ '/site/index' ], null );?>
+        <?=Html::a( "<img class='logo' src='" . Yii::getAlias( '@images' ) . "/logo.png'>", [ '/' ], null );?>
     </div>
 </header>
